ccec9ea5dc7a837e4b1692014c68d7bf0de2dd32
KotaReid <reid161@live.com>
2021-12-06 17:21:14 -0500

    Day 6 Complete.

---
+++ AdventLib/Day6.fs
@@ -0,0 +1,35 @@
+﻿namespace AdventLib
+
+module Day6 = 
+    let toFishCountArray fish = 
+        let fishCounts = Array.create 9 0L
+
+        fish 
+        |> List.countBy id
+        |> List.iter (fun (timer, count) -> fishCounts[timer] <- count)
+
+        fishCounts
+        
+    let simulateDay (fishCounts: array<int64>) =
+        let fishToSpawn = fishCounts[0]
+
+        fishCounts[0] <- fishCounts[1]
+        fishCounts[1] <- fishCounts[2]
+        fishCounts[2] <- fishCounts[3]
+        fishCounts[3] <- fishCounts[4]
+        fishCounts[4] <- fishCounts[5]
+        fishCounts[5] <- fishCounts[6] 
+        fishCounts[6] <- fishCounts[7] + fishToSpawn
+        fishCounts[7] <- fishCounts[8]
+        fishCounts[8] <- fishToSpawn
+
+        fishCounts
+
+    let simulateDays days fish =
+        let fishCounts = fish |> toFishCountArray
+
+        {1..days}
+        |> Seq.toList
+        |> List.fold (fun f _ ->  simulateDay f) fishCounts
+        |> Array.sum
+            
---
+++ AdventTests/Day6Tests.fs
@@ -0,0 +1,54 @@
+﻿namespace AdventTests
+
+open NUnit.Framework
+open TestUtils
+open AdventLib.Day6
+
+[<TestFixture>]
+type Day6Tests() = 
+    let exampleFish = [3;4;3;1;2]
+    let problemFish = 
+        [
+            4;5;3;2;3;3;2;4;2;1
+            2;4;5;2;2;2;4;1;1;1
+            5;1;1;2;5;2;1;1;4;4
+            5;5;1;2;1;1;5;3;5;2
+            4;3;2;4;5;3;2;1;4;1
+            3;1;2;4;1;1;4;1;4;2
+            5;1;4;3;5;2;4;5;4;2
+            2;5;1;1;2;4;1;4;4;1
+            1;3;1;2;3;2;5;5;1;1
+            5;2;4;2;2;4;1;1;1;4
+            2;2;3;1;2;4;5;4;5;4
+            2;3;1;4;1;3;1;2;3;3
+            2;4;3;3;3;1;4;2;3;4
+            2;1;5;4;2;4;4;3;2;1
+            5;3;1;4;1;1;5;4;2;4
+            2;2;4;4;4;1;4;2;4;1
+            1;3;5;1;5;5;1;3;2;2
+            3;5;3;1;1;4;4;1;3;3
+            3;5;1;1;2;5;5;5;2;4
+            1;5;1;2;1;1;1;4;3;1
+            5;2;3;1;3;1;4;1;3;5
+            4;5;1;3;4;2;1;5;1;3
+            4;5;5;2;1;2;1;1;1;4
+            3;1;4;2;3;1;3;5;1;4
+            5;3;1;3;3;2;2;1;5;5
+            4;3;2;1;5;1;3;1;3;5
+            1;1;2;1;1;1;5;2;1;1
+            3;2;1;5;5;5;1;1;5;1
+            4;1;5;4;2;4;5;2;4;3
+            2;5;4;1;1;2;4;3;2;1
+        ]
+
+    let data = dataFromSource exampleFish problemFish
+
+    [<TestCase(DataSource.Example, ExpectedResult = 5934L)>]
+    [<TestCase(DataSource.Problem, ExpectedResult = 353079L)>]
+    member this.ShouldSolvePart1(dataSource) =
+        data dataSource |> simulateDays 80
+
+    [<TestCase(DataSource.Example, ExpectedResult = 26984457539L)>]
+    [<TestCase(DataSource.Problem, ExpectedResult = 1605400130036L)>]
+    member this.ShouldSolvePart2(dataSource) =
+        data dataSource |> simulateDays 256
