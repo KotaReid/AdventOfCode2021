f6628889ee3122fb3260d8acb24892ec41257885
KotaReid <reid161@live.com>
2021-12-05 21:20:28 -0500

    Day 5 Complete

---
+++ AdventLib/Day5.fs
@@ -0,0 +1,60 @@
+﻿namespace AdventLib
+
+module Day5 = 
+    type Orientation =
+        | ZeroSlope
+        | Diagonal
+
+    type Point = {X:int; Y:int;}
+
+    let createPoint x y = { X = x; Y = y}
+
+    let parseLine (line:string) = 
+        line.Split(" -> ") 
+        |> Array.map (fun s -> s.Split ',')
+
+    let parsePoint p = 
+        {
+            X = p |> Array.head |> int
+            Y = p |> Array.last |> int
+        }
+
+    let range a b =
+        if a <= b then 
+            seq { a..b } 
+        else 
+            seq { a .. -1 .. b}
+
+    let pointsFromLine (line:string) =
+        let l = parseLine line
+        let sp = l |> Array.head |> parsePoint
+        let ep = l |> Array.last |> parsePoint
+
+        let xSeq = range sp.X ep.X
+        let ySeq = range sp.Y ep.Y
+
+        if sp.X = ep.X then 
+            (ySeq |> Seq.map (fun y -> {X = sp.X; Y = y}), ZeroSlope)
+        else if sp.Y = ep.Y then
+            (xSeq |> Seq.map (fun x -> {X = x; Y = sp.Y}), ZeroSlope)
+        else
+            (Seq.zip xSeq ySeq |> Seq.map (fun (x,y) -> {X = x; Y = y}), Diagonal)
+
+    let safePoints p =
+        p
+        |> Seq.map fst 
+        |> Seq.concat
+        |> Seq.countBy id
+        |> Seq.filter (fun (_, count) -> count >= 2)
+        |> Seq.length
+
+    let safePointsPart1 (lines:List<string>) = 
+        lines 
+        |> List.map pointsFromLine
+        |> Seq.filter (fun (_, o) -> o = ZeroSlope)
+        |> safePoints 
+
+    let safePointsPart2 (lines:List<string>) = 
+        lines 
+        |> List.map pointsFromLine
+        |> safePoints 
---
+++ AdventTests/Day5Tests.fs
@@ -0,0 +1,22 @@
+﻿namespace AdventTests
+
+open NUnit.Framework
+open TestUtils
+open AdventLib.Day5
+
+[<TestFixture>]
+type Day5Tests() = 
+    let exampleGame = FileUtils.readLines "Day5Example"
+    let problemGame = FileUtils.readLines "Day5"
+
+    let data = dataFromSource exampleGame problemGame
+
+    [<TestCase(DataSource.Example, ExpectedResult = 5)>]
+    [<TestCase(DataSource.Problem, ExpectedResult = 5585)>]
+    member this.ShouldSolvePart1(dataSource) =
+        data dataSource |> safePointsPart1
+
+    [<TestCase(DataSource.Example, ExpectedResult = 12)>]
+    [<TestCase(DataSource.Problem, ExpectedResult = 17193)>]
+    member this.ShouldSolvePart2(dataSource) =
+        data dataSource |> safePointsPart2
