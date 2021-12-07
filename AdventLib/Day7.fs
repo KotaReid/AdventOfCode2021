f09a05858e2e11a1e558d951a015dc21898c14a1
KotaReid <reid161@live.com>
2021-12-07 02:46:34 -0500

    Day 7 Complete. Spent some extra time cleaning this one.

---
+++ AdventLib/Day7.fs
@@ -0,0 +1,40 @@
+﻿namespace AdventLib
+
+module Day7 =
+    let delta a b = 
+        a - b |> abs
+
+    let sumTo a b =
+         let d = delta a b
+         {1..d} |> Seq.sum
+
+    let constantFuels pos crabs = crabs |> List.sumBy (fun c -> delta c pos)
+    let variableFuels pos crabs = crabs |> List.sumBy (fun c -> sumTo c pos)
+
+    let minFuelBy f l = l |> List.map f |> List.min
+    let minFuelByConstant l = minFuelBy (fun pos -> constantFuels pos l)
+    let minFuelByVariable l = minFuelBy (fun pos -> variableFuels pos l)
+
+    let minFuelConstant crabs =
+        crabs
+        |> List.distinct
+        |> minFuelByConstant crabs
+
+    let minFuelChanging crabs =
+        let twoSmallestFuels = 
+            crabs 
+            |> List.distinct
+            |> List.map (fun pos -> (variableFuels pos crabs, pos))
+            |> List.sortBy fst
+            |> List.take 2
+
+        let pos1 = snd twoSmallestFuels[0]
+        let pos2 = snd twoSmallestFuels[1]
+        let d = delta pos1 pos2
+
+        if d = 1 then 
+            snd twoSmallestFuels[0]
+        else if pos1 <= pos2 then
+            [pos1..pos2] |> minFuelByVariable crabs
+        else
+            [pos1..pos2] |> minFuelByVariable crabs
---
+++ AdventTests/Day7Tests.fs
@@ -0,0 +1,22 @@
+﻿namespace AdventTests
+
+open NUnit.Framework
+open TestUtils
+open AdventLib.Day7
+
+[<TestFixture>]
+type Day7Tests() = 
+    let exampleCrabs = [16;1;2;0;4;2;7;1;2;14]
+    let problemCrabs = FileUtils.readAsNumbers "Day7"
+
+    let data = dataFromSource exampleCrabs problemCrabs
+
+    [<TestCase(DataSource.Example, ExpectedResult = 37)>]
+    [<TestCase(DataSource.Problem, ExpectedResult = 343441)>]
+    member this.ShouldSolvePart1(dataSource) =
+        data dataSource |> minFuelConstant
+
+    [<TestCase(DataSource.Example, ExpectedResult = 168)>]
+    [<TestCase(DataSource.Problem, ExpectedResult = 98925151)>]
+    member this.ShouldSolvePart2(dataSource) =
+        data dataSource |> minFuelChanging
