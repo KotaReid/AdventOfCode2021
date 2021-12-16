d5af96563d9883a87f952d1dee55b26d2969a7b6
KotaReid <reid161@live.com>
2021-12-15 22:44:43 -0500

    Day 10 Complete!

---
+++ AdventLib/Day10.fs
@@ -0,0 +1,82 @@
+﻿namespace AdventLib
+
+module Day10 =
+    type Stack = char list
+    
+    type SyntaxResult =
+        | Corrupted of char
+        | Incomplete of Stack
+    
+    let push stack c = c :: stack
+
+    let pop stack = 
+        match stack with
+        | [] -> None
+        | c::tail -> (c,tail) |> Some
+
+    let closeFromOpen c =
+        match c with
+        | '{' -> '}'
+        | '(' -> ')'
+        | '[' -> ']'
+        | '<' -> '>'
+
+    let parseNavigationLine(line: string) =
+        let rec op stack s = 
+            match s with
+            | [] -> Incomplete(stack)
+            | c::rem -> 
+                if "({[<" |> Seq.contains c then
+                    op (push stack c) rem
+                else 
+                    match pop stack with
+                    | None -> invalidOp "stack should not be empty"
+                    | Some (expected, newStack) -> 
+                        let closeMatch = closeFromOpen expected  
+                        if (c <> closeMatch) then
+                            Corrupted(c)
+                        else
+                            op newStack rem
+        line 
+        |> Seq.toList 
+        |> op List.Empty
+
+    let parseNavigationLines(lines: List<string>) =
+        lines
+        |> List.map (fun l -> l.Trim())
+        |> List.map parseNavigationLine
+
+    let scoreCorrupted c = 
+        match c with
+        | ')' -> 3 
+        | ']' -> 57 
+        | '}' -> 1197 
+        | '>' -> 25137 
+
+    let scoreIncomplete c = 
+        match c with
+        | ')' -> 1L
+        | ']' -> 2L
+        | '}' -> 3L
+        | '>' -> 4L
+
+    let corruptedScore (lines: List<string>) = 
+        lines
+        |> parseNavigationLines
+        |> List.choose (function Corrupted c -> Some c | _ -> None)
+        |> List.sumBy scoreCorrupted
+
+    let incompleteScore (lines: List<string>) = 
+        let scoreForLine stack =
+            stack 
+            |> List.map closeFromOpen
+            |> List.fold (fun t c -> t * 5L + (scoreIncomplete c)) 0L
+
+        let scores = 
+            lines
+            |> parseNavigationLines
+            |> List.choose (function Incomplete stack -> Some stack | _ -> None)
+            |> List.map scoreForLine
+            |> List.sortDescending
+
+        scores[scores.Length / 2]
---
+++ AdventTests/Day10Tests.fs
@@ -0,0 +1,22 @@
+﻿namespace AdventTests
+
+open NUnit.Framework
+open TestUtils
+open AdventLib.Day10
+
+[<TestFixture>]
+type Day10Tests() = 
+    let exampleInput = FileUtils.readStrings "Day10Example"
+    let problemInput = FileUtils.readStrings "Day10"
+
+    let data = dataFromSource exampleInput problemInput
+
+    [<TestCase(DataSource.Example, ExpectedResult = 26397)>]
+    [<TestCase(DataSource.Problem, ExpectedResult = 367227)>]
+    member this.ShouldSolvePart1(dataSource) =
+        data dataSource |> corruptedScore
+
+    [<TestCase(DataSource.Example, ExpectedResult = 288957L)>]
+    [<TestCase(DataSource.Problem, ExpectedResult = 3583341858L)>]
+    member this.ShouldSolvePart2(dataSource) =
+        data dataSource |> incompleteScore
